c6e711c88d39e68acf4e85f4301f33b4fd1a71f5
GalvinFinTech <147152833+GalvinFinTech@users.noreply.github.com>
2024-01-17 16:40:19 +0700

    Update MCH_K214142099.py

--- MCH_K214142099.py
+++ MCH_K214142099.py
@@ -171,7 +171,7 @@ def main():
     # Hiển thị tiêu đề và thông tin ở cột trái
     with left_column:
         st.title('MCH')
-        image = Image.open('/Users/nguyenhoangvi/Downloads/Ứng dụng Python/MCH_K214142099/MCH.jpeg')
+        image = Image.open('MCH.jpeg')
         st.image(image, caption='CTCP Hàng tiêu dùng Masan')
     with right_column:
         # Display metrics in a single row
